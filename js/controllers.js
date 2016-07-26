fffeddb76cb88b0763d6dabeb280bf8337795c4f
wangmingxu <wangmingxu@qipeipu.com>
2016-07-26 16:34:40 +0800

    add

--- js/controllers.js
+++ js/controllers.js
@@ -43,7 +43,9 @@ angular.module('starter.controllers', [])
         })
         .then(function successCallback(data, status, headers, config) {
                 console.log(data.data);
-                $scope.list = data.data.model.showPlanDTOList;
+                $scope.list = data.data.model.showPlanRowVOList;
+                $scope.totalSize = data.data.model.totalSize;
+                console.log(totalSize);
             },
             function errorCallback(data, status, headers, config) {
                 console.log(data);
@@ -118,8 +120,8 @@ angular.module('starter.controllers', [])
     $scope.cacheIpt = function(cache) {
         // var start_time = $filter("date")($scope.start_time, "yyyy-MM-dd");
         // var end_time = $filter("date")($scope.end_time, "yyyy-MM-dd");
-        var start_time=$scope.start_time;
-        var end_time=$scope.end_time;
+        var start_time = $scope.start_time;
+        var end_time = $scope.end_time;
         var admin_isSelect = JSON.stringify($scope.admin_isSelect);
         var serviceArea_selectIndex = JSON.stringify($scope.serviceArea_selectIndex);
         var serviceArea_isSelect = JSON.stringify($scope.serviceArea_isSelect);
@@ -200,54 +202,61 @@ angular.module('starter.controllers', [])
 })
 
 //添加计划步骤二
-.controller('addPlanStep2Ctrl', function($scope, $http, $location, prefix, $state, $cacheFactory) {
+.controller('addPlanStep2Ctrl', function($scope, $http, $location, prefix, $state, $cacheFactory, $filter) {
     // console.log(JSON.parse(cache.get('admin_isSelect')));
     // console.log(JSON.parse(cache.get('serviceArea_isSelect')));
-    // console.log(cache.get('start_time'));
     // $("[data-toggle='popover']").popover();
-    $scope.planName=cache.get('planName');
+    $scope.planName = cache.get('planName');
+    $scope.start_time = $filter("date")(cache.get('start_time'), "yyyy-MM-dd");
+    $scope.end_time = $filter("date")(cache.get('end_time'), "yyyy-MM-dd");
+    $scope.planType = cache.get('planType');
+    $scope.priority = cache.get('priority');
+    $scope.admin_isSelect = JSON.parse(cache.get('admin_isSelect'));
+    $scope.serviceArea_isSelect = JSON.parse(cache.get('serviceArea_isSelect'));
     $scope.getPic = function() {
-        var _url = prefix + "MaterialList";
-        Mock.mock(_url, {
-            'list|6-10': [{
-                'id|+1': 1,
-                'picUrl': "img/defaultimg.png",
-                'materialName|1': ['素材名素材名素素材名素材名1...','素材名素材名素素材名素材名2...','素材名素材名素素材名素材名3...','素材名素材名素素材名素材名4...'],
-            }]
-        });
+        var _url = prefix + "cms/material/query/materials?materialType=1";
+        // Mock.mock(_url, {
+        //     'list|6-10': [{
+        //         'materialId|+1': 1,
+        //         'storageUrl': "img/defaultimg.png",
+        //         'materialName|1': ['素材名素材名素素材名素材名1...','素材名素材名素素材名素材名2...','素材名素材名素素材名素材名3...','素材名素材名素素材名素材名4...'],
+        //     }]
+        // });
         $http({
                 method: 'GET',
                 url: _url
             })
             .then(function successCallback(data, status, headers, config) {
-                    $scope.picList = data.data.list;
+                    // $scope.picList = data.data.list;  //模拟数据
+                    $scope.picList = data.data.model.materialDTOList;   //真实数据
+                    console.log($scope.picList);
                 },
                 function errorCallback(data, status, headers, config) {
                     console.log(data);
                 });
     };
     $scope.getPic();
-    $scope.getResource = function() {
+    $scope.getAdvResource = function() {
         $scope.checked_resource = [];
-        var res_url = prefix + 'cms/resource/query/resource';
-        Mock.mock(res_url, {
-            'list|1-10': [{
-                'id|+1': 1,
-                'resourcePositionName': 'PC首页焦点图轮播',
-                'platform|1': [1, 2, 3, 4],
-                'pixelHeight': 760,
-                'pixelWidth': 450,
-                'size': '128KB'
-            }]
-        });
+        var res_url = prefix + 'cms/resource/advertisement/resources';
+        // Mock.mock(res_url, {
+        //     'list|1-10': [{
+        //         'id|+1': 1,
+        //         'resourcePositionName': 'PC首页焦点图轮播',
+        //         'platform|1': [1, 2, 3, 4],
+        //         'pixelHeight': 760,
+        //         'pixelWidth': 450,
+        //         'size': '128KB'
+        //     }]
+        // });
         $http({
                 method: 'GET',
                 url: res_url
             })
             .then(function successCallback(data, status, headers, config) {
                     console.log(data.data);
-                    // $scope.resourceList = data.data.model.resourcePositionDTOList;      //真实数据
-                    $scope.resourceList = data.data.list; //模拟数据
+                    $scope.resourceList = data.data.model.resourcePositionDTOList; //真实数据
+                    // $scope.resourceList = data.data.list; //模拟数据
                 },
                 function errorCallback(data, status, headers, config) {
                     console.log(data);
@@ -256,14 +265,14 @@ angular.module('starter.controllers', [])
     $scope.resource_ensure = function() {
         $scope.resourceList.map(function(item, i) {
             if (item.isChecked) {
-                item.picList = $scope.picList;
+                item.picList = angular.copy($scope.picList); //!!!!!!引用数据类型要用copy
                 $scope.checked_resource.push(item);
             }
         });
     };
     $scope.show_picList = function(i) {
-        $scope.temp_index=i;
-        $scope.temp_picList=$scope.checked_resource[i].picList;
+        $scope.temp_index = i;
+        $scope.temp_picList = $scope.checked_resource[i].picList;
     };
     $scope.select_pic = function(i) {
         if ($scope.temp_picList[i].isSelect) {
@@ -291,44 +300,89 @@ angular.module('starter.controllers', [])
             return false;
         }
     };
-    $scope.pic_next = function() {
-        // var i=$scope.temp_index;
-        // $scope.checked_resource[i].picList=$scope.temp_picList;
-        $scope.temp_picList_next=[];
-        $scope.temp_picList.map(function(item, i) {
-          if(item.isSelect){
-            $scope.temp_picList_next.push(item);
-          }
+    $scope.pic_next = function(i) {
+        $scope.temp_picList_next = [];
+        var temp_index;
+        if(i!=undefined){
+          $scope.temp_index=i;
+        }
+        $scope.checked_resource[$scope.temp_index].picList.map(function(item, i) {
+            if (item.isSelect) {
+                $scope.temp_picList_next.push(item);
+            }
         });
-        console.log($scope.temp_picList_next);
-        $('#selectPic_Modal').modal('hide');
-        $('#editPic_Modal').modal('show');
+        if (!i) {
+            $('#selectPic_Modal').modal('hide');
+            $('#editPic_Modal').modal('show');
+        }else{
+          $('#editPic_Modal').modal('show');
+        }
     };
-    $scope.swapItems=function(arr,index1,index2){
-      arr[index1]=arr.splice(index2,1,arr[index1])[0];
-      return arr;
+    $scope.swapItems = function(arr, index1, index2) {
+        arr[index1] = arr.splice(index2, 1, arr[index1])[0];
+        return arr;
     };
-    $scope.move_up=function(arr,$index){
-      if($index==0){
-        return;
-      }
-      $scope.swapItems(arr,$index,$index-1);
+    $scope.move_up = function(arr, $index) {
+        if ($index == 0) {
+            return;
+        }
+        $scope.swapItems(arr, $index, $index - 1);
     };
-    $scope.move_down=function(arr,$index){
-      if($index==arr.length-1){
-        return;
-      }
-      $scope.swapItems(arr,$index,$index+1);
+    $scope.move_down = function(arr, $index) {
+        if ($index == arr.length - 1) {
+            return;
+        }
+        $scope.swapItems(arr, $index, $index + 1);
     };
-    $scope.pic_finish=function(){
-
+    $scope.picSelect_finish = function() {
+        var i = $scope.temp_index;
+        $scope.checked_resource[i].picList = angular.copy($scope.temp_picList_next);
+        console.log($scope.checked_resource);
+    };
+    $scope.picPlan_finish = function() {
+        console.log($scope.checked_resource);
+        $scope.resourcePositionMaterialReleaseVOList=[];
+        $scope.checked_resource.map(function(item,i){
+          var obj={};
+          obj.resourcePositionId=item.resourcePositionId;
+          obj.materialIdList=[];
+          item.picList.map(function(item,i){
+            obj.materialIdList.push(item.materialId);
+          });
+          $scope.resourcePositionMaterialReleaseVOList.push(obj);
+        });
+        console.log($scope.resourcePositionMaterialReleaseVOList);
+        var data = {
+            showPlanName: $scope.planName,
+            showPlanType: $scope.planType,
+            displayBeginTime: $scope.start_time,
+            displayEndTime: $scope.end_time,
+            priority: $scope.priority,
+            provinceIdList:$scope.admin_isSelect,
+            cityConfigIdList:$scope.serviceArea_isSelect,
+            resourcePositionMaterialReleaseVOList:$scope.resourcePositionMaterialReleaseVOList
+        };
+        console.log(data);
+        var _url=prefix+"cms/showPlan/release";
+        $http({
+                method: 'POST',
+                url: _url,
+                data: data
+            })
+            .then(function successCallback(data, status, headers, config) {
+                    $scope.articleList = data.data.list;
+                    console.log(data.data);
+                },
+                function errorCallback(data, status, headers, config) {
+                    console.log(data);
+                });
     };
     $scope.getArticle = function() {
-        var _url = prefix + "MaterialList";
+        var _url = prefix + "cms/material/query/materials?materialType=2";
         Mock.mock(_url, {
             'list|1-10': [{
                 'id|+1': 1,
-                'title|1': ['注册与审核', '用户登录', '用户注册'],
+                'meterialName|1': ['注册与审核', '用户登录', '用户注册'],
                 'previewUrl': 'https://www.baidu.com/img/baidu_jgylogo3.gif',
             }]
         });
@@ -372,7 +426,7 @@ angular.module('starter.controllers', [])
         }
         fd.append("targetUrl", $scope.targetUrl);
         //  var _url=prefix+"uploadpic"
-        var _url = "http://192.168.17.9:8088/cms/material/upload/pictures";
+        var _url = prefix+"cms/material/upload/pictures";
         $.ajax({
                 url: _url,
                 type: 'POST',
@@ -403,17 +457,20 @@ angular.module('starter.controllers', [])
     var ue = UE.getEditor('editor');
     $scope.getContent = function() {
         var arr = [];
+        var content = UE.getEditor('editor').getContent();
         arr.push("使用editor.getContent()方法可以获得编辑器的内容");
         arr.push("内容为：");
-        arr.push(UE.getEditor('editor').getContent());
-        alert(arr.join("\n"));
+        arr.push(content);
+        // alert(arr.join("\n"));
+        console.log(arr.join("\n"));
+        return content;
     };
 })
 
 //广告资源位Ctrl
 .controller('advResourceCtrl', function($scope, $http, $location, prefix, $stateParams) {
     $scope.platform = $stateParams.platform;
-    var _url = prefix + "cms/resource/query/resource";
+    var _url = prefix + "cms/resource/advertisement/resources";
     // Mock.mock(_url, {
     //     'list|1-10': [{
     //         'id|+1': 1,
@@ -435,4 +492,8 @@ angular.module('starter.controllers', [])
             function errorCallback(data, status, headers, config) {
                 console.log(data);
             });
+})
+
+.controller('articleResourceCtrl', function($scope, $http, $location, prefix, $stateParams) {
+
 })
